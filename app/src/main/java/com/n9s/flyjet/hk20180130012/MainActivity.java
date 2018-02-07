d8ea565ae82812a67ece4911ef8988c93bc96264
HKW <HKW@yahoo.com>
2018-02-07 14:03:32 +0800

    Google Map_Single Trigger

---
+++ app/src/main/java/com/n9s/flyjet/hk20180130012/MainActivity.java
@@ -0,0 +1,124 @@
+package com.n9s.flyjet.hk20180130012;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.IOException;
+import java.util.List;
+
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+public class MainActivity extends AppCompatActivity
+{
+    LocationManager lm;
+    Address addr;
+    MyListener listener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lm = (LocationManager) getSystemService(LOCATION_SERVICE);
+        listener = new MyListener();
+
+    }
+
+    public void click1(View v) {
+        {
+            if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION, ACCESS_COARSE_LOCATION}, 321);
+                return;
+            } else {
+                startLoc();
+            }
+        }
+    }
+
+        @Override
+        public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+        @NonNull int[] grantResults)
+        {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+            if (requestCode == 321)
+            {
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    startLoc();
+                }
+                else
+                {
+                }
+            }
+        }
+
+
+    public void startLoc()
+    {
+        if (ActivityCompat.checkSelfPermission(this, ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, listener);
+    }
+
+    class MyListener implements LocationListener
+    {
+        @Override
+        public void onLocationChanged(Location location)
+        {
+            Log.d("LOC", "Change!! " + location.getLatitude() + "," + location.getLongitude());
+            Location loc101 = new Location("LOC");
+            loc101.setLatitude(25.0336);
+            loc101.setLongitude(121.5646);
+
+            float dist = location.distanceTo(loc101);
+            Log.d("LOC", "Dist:" + dist);
+
+            Geocoder geocoder = new Geocoder(MainActivity.this);
+            try
+            {
+                List<Address> mylist2 = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+                lm.removeUpdates(listener);
+                addr = mylist2.get(0);
+                Log.d("LOC", addr.getAddressLine(0));
+
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle)
+        {
+
+        }
+        @Override
+        public void onProviderEnabled(String s)
+        {
+
+        }
+        @Override
+        public void onProviderDisabled(String s)
+        {
+
+        }
+    }
+
+}
